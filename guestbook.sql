1dc39e594d1806da8e85ffe2b274e32bb5878750
raxniper <raxniper@naver.com>
2020-07-03 10:52:03 +0900

    sql update

--- guestbook.sql
+++ guestbook.sql
@@ -27,6 +27,14 @@ values (seq_guestbook_no.nextval,
         '1234', 
         '방문합니다.',
         sysdate);
+        
+insert into guestbook values (seq_guestbook_no.nextval, '김현수', '1234', '김현수 방문합니다.', sysdate);
+insert into guestbook values (seq_guestbook_no.nextval, '진헌태', '1234', '진헌태 방문합니다.', sysdate);
+insert into guestbook values (seq_guestbook_no.nextval, '이현준', '1234', '이현준 방문합니다.', sysdate);
+insert into guestbook values (seq_guestbook_no.nextval, '김태석', '1234', '김태석 방문합니다.', sysdate);
+insert into guestbook values (seq_guestbook_no.nextval, '김지성', '1234', '김지성 방문합니다.', sysdate);
+insert into guestbook values (seq_guestbook_no.nextval, '김우성', '1234', '김우성 방문합니다.', sysdate);
+insert into guestbook values (seq_guestbook_no.nextval, '안용준', '1234', '안용준 방문합니다.', sysdate);        
  
 commit;
 
